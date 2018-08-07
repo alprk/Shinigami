f1ab5a92cd0b8b0c1158c2a2120399f57cd32a14
Antoine PALIOTTI <antoine.paliotti@gmail.com>
2018-08-07 11:19:49 +0200

    Add Customer entity

---
+++ src/Entity/Customer.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\User;
+
+/**
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\CustomerRepository")
+ */
+class Customer extends User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nickname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adress;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $birthdate;
+
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Center")
+     * @ORM\JoinColumn(name="center_id",referencedColumnName="id")
+     */
+    private $center;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Card")
+     * @ORM\JoinColumn(name="card_id",referencedColumnName="id")
+     */
+    private $card;
+
+    public $encoder;
+
+    /**
+     * UserFactory constructor.
+     * @param UserPasswordEncoderInterface $encoder
+     */
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNickname(): ?string
+    {
+        return $this->nickname;
+    }
+
+    public function setNickname(string $nickname): self
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getBirthdate(): ?\DateTimeInterface
+    {
+        return $this->birthdate;
+    }
+
+    public function setBirthdate(\DateTimeInterface $birthdate): self
+    {
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCard()
+    {
+        return $this->card;
+    }
+
+    /**
+     * @param mixed $card
+     */
+    public function setCard($card): void
+    {
+        $this->card = $card;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCenter()
+    {
+        return $this->center;
+    }
+
+    /**
+     * @param mixed $center
+     */
+    public function setCenter($center): void
+    {
+        $this->center = $center;
+    }
+
+
+}
